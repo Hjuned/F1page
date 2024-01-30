a6f5a6d51bf438823d606acdc8e1954f6bd254c0
Hjuned <hjuned@outlook.com>
2024-01-30 13:22:17 +0000

    test

--- f1data.js
+++ f1data.js
@@ -3,7 +3,7 @@ import {createDriverStandingsPage} from `${baseURL}/driverstandings/driverStandi
 import {createConstructorsStandingsPage} from `${baseURL}/constructorStandings/constructorStandings.js`;
 import {createResultsPage } from `${baseURL}/raceResults/raceResults.js`;
 import {createSchedulePage } from `${baseURL}/raceSchedule/raceSchedule.js`;
-import {createHomePage } from `${baseURL}/homePage.js`;
+import {createHomePage } from `${baseURL}/F1Page/homePage.js`;
 
 
 // abstracted function
